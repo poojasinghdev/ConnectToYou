72755dfbac897bff59dc4bb32acc9f5ef399593f
poojasinghdev <singhpooja1629@gmail.com>
2020-09-28 13:26:14 +0530

    Added Routes details

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
