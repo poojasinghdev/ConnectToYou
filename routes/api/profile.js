95f4734abdf3623fc91d3bcf88afa5c311407169
poojasinghdev <singhpooja1629@gmail.com>
2020-09-29 10:42:31 +0530

    added Profile models and routes details

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route    Get api/profile
-// @desc     Test Route
-// @access   Public
+const Profile = require('../../models/Profile');
+// @route    Get api/profile/me
+// @desc     Get current users profile
+// @access   Private
 
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/me', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
